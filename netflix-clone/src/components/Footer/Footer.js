17db965ab1f6a3ab83d91e58e9bb5175a2021d4a
Firezer27 <firezer.bizuwork27@gmail.com>
2024-11-18 17:04:36 +0300

    some configuration of footer.css

--- netflix-clone/src/components/Footer/Footer.js
+++ netflix-clone/src/components/Footer/Footer.js
@@ -2,6 +2,7 @@ import React from "react";
 import FacebookOutLinedIcon from "@mui/icons-material/FacebookOutlined";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import YoutubeIcon from "@mui/icons-material/YouTube";
+import './footer.css'
 
 import "./footer.css";
 const Footer = () => {
@@ -21,7 +22,32 @@ const Footer = () => {
               <li>Legal Notice</li>
             </ul>
           </div>
+          <div>
+            <ul>
+              <li>Help Center</li>
+              <li>Jobs</li>
+              <li>Cookie Preferences</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Gift Cards</li>
+              <li>Terms of Use</li>
+              <li>Corporate Information</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Media Center</li>
+              <li>Privacy</li>
+              <li>Contact Use</li>
+            </ul>
+          </div>
         </div>
+        <div className="service_code">
+          <p>Service Code</p>
+        </div>
+        <div className="copy-write">&copy; 1997-2024 Netflix, Inc.</div>
       </div>
     </div>
   );
